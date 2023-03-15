5d724ba2b9d37353d3eea83fa454cca9718ddf78
xinyew <wangxinye1996@outlook.com>
2023-03-15 15:03:36 -0400

    converted enter and newGame to active low signas

--- 05_testbench.sv
+++ 05_testbench.sv
@@ -3,7 +3,7 @@
 module testbench();
   logic [3:0] cMove, hMove;
   logic [3:0] h3, h2, h1, h0, c3, c2, c1, c0;
-  logic win, clock, reset, enter, newGame;
+  logic win, clock, reset, enter_L, newGame_L;
 
   task5 DUT (.*);
   
@@ -23,17 +23,17 @@ module testbench();
   initial begin
     // C_5
     hMove <= 4'd4;
-    enter <= 1'd0;
+    enter_L <= 1'd0;
     @(posedge clock) // #10 C_5
     @(posedge clock) // #30 C_5
-    enter <= 1'd1;
+    enter_L <= 1'd1;
     @(posedge clock) // #50 C_5
     hMove <= 4'd6;
     @(posedge clock) // #70 C_5
-    enter <= 1'd0;
+    enter_L <= 1'd0;
     @(posedge clock) // #90 C_5_H_6_E
     @(posedge clock) // #110 C_5_H_6_E
-    enter <= 1'd1;
+    enter_L <= 1'd1;
     @(posedge clock) // #130 C_1_5_H_6
 
     #1 $finish;
