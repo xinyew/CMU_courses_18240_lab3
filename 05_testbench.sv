6e9c80a65a721f2283fbd38a29b733374b72fc11
xinyewWSL <wangxinye1996@gmail.com>
2023-03-15 13:52:58 -0400

    start writing testbench for part5

--- 05_testbench.sv
+++ 05_testbench.sv
@@ -8,7 +8,9 @@ module testbench();
   task5 DUT (.*);
   
   initial begin
-    $monitor($time,, "state: %s, cMove: %b, hMove: %b, win: %b, h3: %d, h2: %d, h1: %d, h0: %d, c3: %d, c2: %d, c1: %d, c0: %d",
+    $monitor($time,, "state: %s, cMove: %b, hMove: %b, win: %b \
+                      h3: %d, h2: %d, h1: %d, h0: %d, c3: %d, c2: %d, c1: %d, c0: %d",
                       DUT.currState.name, cMove, hMove, win,
                       h3, h2, h1, h0, c3, c2, c1, c0);
+  end
 endmodule : testbench
