5cbc1a45b6c68dace663d9cbbd6f0ef861f48433
xinyew <wangxinye1996@outlook.com>
2023-03-14 21:53:26 -0400

    half done

--- theBigGame.sv
+++ theBigGame.sv
@@ -63,42 +63,463 @@ module task5 (
             nextState = C_5_H_6_E;
         end
       end
-      C_5_I
+      C_1_5_H_6: begin
+        if (enter)
+          unique case (hMove)
+            4'd1,
+            4'd5,
+            4'd6: nextState = C_1_5_H_6_I;
+            4'd2: nextState = C_1_5_H_2_6_E;
+            4'd3: nextState = C_1_5_H_3_6_E;
+            4'd4: nextState = C_1_5_H_4_6_E;
+            4'd7: nextState = C_1_5_H_6_7_E;
+            4'd8: nextState = C_1_5_H_6_8_E;
+            4'd9: nextState = C_1_5_H_6_9_E;
+          endcase
+      end
+      C_1_3_5_H_6_9:
+        if (enter)
+          unique case (hMove)
+            4'd1,
+            4'd3,
+            4'd5,
+            4'd6,
+            4'd9: nextState = C_1_3_5_H_6_9_I;
+            4'd2: nextState = C_1_3_5_H_2_6_9_E;
+            4'd4: nextState = C_1_3_5_H_4_6_9_E;
+            4'd7: nextState = C_1_3_5_H_6_7_9_E;
+            4'd8: nextState = C_1_3_5_H_6_8_9_E;
+          endcase
+       
+      C_5_H_6_E:
+        if (~enter)
+          nextState = C_1_5_H_6;
+      C_1_5_H_2_6_E:
+        if (~enter)
+          nextState = C_1_5_9_H_2_6_W;
+      C_1_5_H_3_6_E:
+        if (~enter)
+          nextState = C_1_5_9_H_3_6_W;
+      C_1_5_H_4_6_E:
+        if (~enter)
+          nextState = C_1_5_9_H_4_6_W;
+      C_1_5_H_6_7_E:
+        if (~enter)
+          nextState = C_1_5_9_H_6_7_W;
+      C_1_5_H_6_8_E:
+        if (~enter)
+          nextState = C_1_5_9_H_6_8_W;
+      C_1_5_H_6_9_E:
+        if (~enter)
+          nextState = C_1_3_5_H_6_9;
+      C_1_3_5_H_2_6_9_E:
+        if (~enter)
+          nextState = C_1_3_5_7_H_2_6_9_W;
+      C_1_3_5_H_4_6_9_E:
+        if (~enter)
+          nextState = C_1_2_3_5_H_4_6_9_W;
+      C_1_3_5_H_6_7_9_E:
+        if (~enter)
+          nextState = C_1_2_3_5_H_6_7_9_W;
+      C_1_3_5_H_6_8_9_E:
+        if (~enter)
+          nextState = C_1_2_3_5_H_6_8_9_W;
+      
+      C_1_5_9_H_2_6_W:
+        if (newGame)
+          nextState = C_1_5_9_H_2_6_W_N
+      C_1_5_9_H_3_6_W:
+        if (newGame)
+          nextState = C_1_5_9_H_3_6_W_N
+      C_1_5_9_H_4_6_W:
+        if (newGame)
+          nextState = C_1_5_9_H_4_6_W_N
+      C_1_5_9_H_6_7_W:
+        if (newGame)
+          nextState = C_1_5_9_H_6_7_W_N
+      C_1_5_9_H_6_8_W:
+        if (newGame)
+          nextState = C_1_5_9_H_6_8_W_N
+      C_1_3_5_7_H_2_6_9_W:
+        if (newGame)
+          nextState = C_1_3_5_7_H_2_6_9_W_N;
+      C_1_2_3_5_H_4_6_9_W:
+        if (newGame)
+          nextState = C_1_2_3_5_H_4_6_9_W_N;
+      C_1_2_3_5_H_6_7_9_W:
+        if (newGame)
+          nextState = C_1_2_3_5_H_6_7_9_W_N;
+      C_1_2_3_5_H_6_8_9_W:
+        if (newGame)
+          nextState = C_1_2_3_5_H_6_8_9_W_N;
+      
+      C_5_I,
+      C_1_5_H_6_I,
+      C_1_3_5_H_6_9_I:
+        if (~enter)
+          nextState = C_5;
+
+      C_1_5_9_H_2_6_W_N,
+      C_1_5_9_H_3_6_W_N,
+      C_1_5_9_H_4_6_W_N,
+      C_1_5_9_H_6_7_W_N,
+      C_1_5_9_H_6_8_W_N,
+      C_1_2_3_5_H_4_6_9_W_N,
+      C_1_2_3_5_H_6_7_9_W_N,
+      C_1_2_3_5_H_6_8_9_W_N,
+      C_1_3_5_7_H_2_6_9_W_N:
+        if (~newGame)
+          nextState = C_5;
     endcase
   end
 
   
   // output generation
   always_comb begin
-	 cMove = 4'b0000;
-	 win = 1'b1;
-    if (currState == S0) begin
-      cMove = 4'h5;
-      win = 0;
-    end
-    if (currState == S1) begin
-      cMove = 4'h1;
-      win = 0;
-    end
-    if (currState == S2) begin
-      cMove = 4'h9;
-      win = 1;
-    end
-    if (currState == S3) begin
-      cMove = 4'h3;
-      win = 0;
-    end
-    if (currState == S4) begin
-      cMove = 4'h2;
-      win = 1;
-    end
-    if (currState == S5) begin
-      cMove = 4'h7;
-      win = 1;
-    end
-  end
+    cMove = 4'b0000;
+    win = 1'b1;
+      {c3, c2, c1, c0, h3, h2, h1, h0} = 32'b0;
+    if (currState == C_5) begin
+        cMove = 4'h5;
+        win = 0;
+        c3 = 4'd5;
+      end
 
-  // register
+      if (currState == C_5_I) begin
+        cMove = 4'h5;
+        win = 0;
+        c3 = 4'd5;
+      end
+
+      if (currState == C_5_H_6_E) begin
+        cMove = 4'h5;
+        win = 0;
+        c3 = 4'd5;
+      end
+
+      if (currState == C_1_5_H_6) begin
+        cMove = 4'h1;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        h3 = 4'd6;
+      end
+
+      if (currState == C_1_5_H_6_I) begin
+        cMove = 4'h1;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        h3 = 4'd6;
+      end
+
+
+      if (currState == C_1_5_H_2_6_E) begin
+        cMove = 4'h1;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        h3 = 4'd6;
+      end
+      if (currState == C_1_5_H_3_6_E) begin
+        cMove = 4'h1;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        h3 = 4'd6;
+      end
+      if (currState == C_1_5_H_4_6_E) begin
+        cMove = 4'h1;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        h3 = 4'd6;
+      end
+      if (currState == C_1_5_H_6_7_E) begin
+        cMove = 4'h1;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        h3 = 4'd6;
+      end
+      if (currState == C_1_5_H_6_8_E) begin
+        cMove = 4'h1;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        h3 = 4'd6;
+      end
+      if (currState == C_1_5_H_6_9_E) begin
+        cMove = 4'h1;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        h3 = 4'd6;
+      end
+
+
+      if (currState == C_1_5_H_2_6_W) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        c1 = 4'd7;
+        h3 = 4'd2;
+        h2 = 4'd6;
+      end
+      if (currState == C_1_5_H_2_6_W_N) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        c1 = 4'd7;
+        h3 = 4'd2;
+        h2 = 4'd6;
+      end
+
+      if (currState == C_1_5_H_3_6_W) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        c1 = 4'd7;
+        h3 = 4'd3;
+        h2 = 4'd6;
+      end
+      if (currState == C_1_5_H_3_6_W_N) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        c1 = 4'd7;
+        h3 = 4'd3;
+        h2 = 4'd6;
+      end
+
+      if (currState == C_1_5_H_4_6_W) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        c1 = 4'd7;
+        h3 = 4'd4;
+        h2 = 4'd6;
+      end
+      if (currState == C_1_5_H_4_6_W_N) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd5;
+        c1 = 4'd7;
+        h3 = 4'd4;
+        h2 = 4'd6;
+      end
+
+      if (currState == C_1_5_H_6_7_W) begin
+        cMove = 4'h2;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd2;
+        c1 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd7;
+      end
+      if (currState == C_1_5_H_6_7_W_N) begin
+        cMove = 4'h2;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd2;
+        c1 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd7;
+      end
+
+
+
+      if (currState == C_1_5_H_6_8_W) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd2;
+        c1 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd8;
+      end
+      if (currState == C_1_5_H_6_8_W_N) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd2;
+        c1 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd8;
+      end
+      
+      
+      if (currState == C_1_3_5_H_6_9) begin
+        cMove = 4'h3;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd9;
+      end
+      if (currState == C_1_3_5_H_6_9_I) begin
+        cMove = 4'h3;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_2_6_9_E) begin
+        cMove = 4'h3;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        h3 = 4'd2;
+        h2 = 4'd6;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_4_6_9_E) begin
+        cMove = 4'h3;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        h3 = 4'd4;
+        h2 = 4'd6;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_6_7_9_E) begin
+        cMove = 4'h3;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd7;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_6_8_9_E) begin
+        cMove = 4'h3;
+        win = 0;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd8;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_4_6_9_W) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        c0 = 4'd7;
+        h3 = 4'd4;
+        h2 = 4'd6;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_4_6_9_W_N) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        c0 = 4'd7;
+        h3 = 4'd4;
+        h2 = 4'd6;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_6_7_9_W) begin
+        cMove = 4'h2;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd2;
+        c1 = 4'd3;
+        c0 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd7;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_6_7_9_W_N) begin
+        cMove = 4'h2;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd2;
+        c1 = 4'd3;
+        c0 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd7;
+        h1 = 4'd9;
+        
+      end
+
+      if (currState == C_1_3_5_H_6_8_9_W) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd2;
+        c1 = 4'd3;
+        c0 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd8;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_6_8_9_W_N) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd2;
+        c1 = 4'd3;
+        c0 = 4'd5;
+        h3 = 4'd6;
+        h2 = 4'd8;
+        h1 = 4'd9;
+
+      end
+
+      if (currState == C_1_3_5_H_2_6_9_W) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        c0 = 4'd7;
+        h3 = 4'd2;
+        h2 = 4'd6;
+        h1 = 4'd9;
+      end
+
+      if (currState == C_1_3_5_H_2_6_9_W_N) begin
+        cMove = 4'h7;
+        win = 1;
+        c3 = 4'd1;
+        c2 = 4'd3;
+        c1 = 4'd5;
+        c0 = 4'd7;
+        h3 = 4'd2;
+        h2 = 4'd6;
+        h1 = 4'd9;
+      end
+    end
+
+
+    // register
   always_ff @(posedge clock)
     if (reset)
       currState <= S0;
