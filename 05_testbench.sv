4844f84ea237afefa4ace1171bbff5f3af3944e8
xinyewWSL <wangxinye1996@gmail.com>
2023-03-15 13:36:37 -0400

    start writing testbench for part5

---
+++ 05_testbench.sv
@@ -0,0 +1,10 @@
+`default_nettype none
+
+module testbench();
+  logic [3:0] cMove, hMove;
+  logic [3:0] h3, h2, h1, h0, c3, c2, c1, c0;
+  logic win, clock, reset, enter, newGame;
+
+  task5 DUT (.*)
+
+endmodule : testbench
