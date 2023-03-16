51731477c7cf96bcbe4ecba02e6c41a9563bd876
xinyewWSL <wangxinye1996@gmail.com>
2023-03-16 00:50:06 -0400

    remove files

--- 05_chipInterface.sv
+++ 05_chipInterface.sv
@@ -1,14 +1,21 @@
 `default_nettype none // Required in every sv file
-module dFlipFlopcI(
-  output logic q,
-  input  logic d, clock, reset);
 
-  always_ff @(posedge clock, posedge reset)
-    if (reset == 1'b1)
-      q <= 0;
+module Dflipflop
+  (input  logic D,
+   input  logic clock, preset, reset,
+   output logic Q);
+  
+  always_ff @(posedge clock)
+    if (reset && preset)
+      Q <= 1'bx;
+    else if (reset)
+      Q <= 1'b0;
+    else if (preset)
+      Q <= 1'b1;
     else
-      q <= d;
-endmodule : dFlipFlopcI
+      Q <= D;
+
+endmodule : Dflipflop
 
 module chipInterface
   (input logic [3:0] KEY,
@@ -17,11 +24,24 @@ module chipInterface
   output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, HEX6, HEX7,
   output logic [7:0] LEDG,
   output logic [3:0] LEDR);
-  logic [3:0] h_3, h_2, h_1, h_0, c_3, c_2, c_1, c_0;
-  
-  logic ww;
 
-  task5(.newGame_L(KEY[0]), 
+  // sync enter_L, newGame_L using clock from FPGA
+  logic enter_L, newGame_L;
+  Dflipflop(.D(KEY[3]),
+            .Q(enter_L),
+            .clock(CLOCK_50),
+            .preset(SW[17]));
+
+  Dflipflop(.D(KEY[0]),
+            .Q(newGame_L),
+            .clock(CLOCK_50),
+            .preset(SW[17]));
+
+  logic [3:0] h_3, h_2, h_1, h_0, c_3, c_2, c_1, c_0;
+  logic ww;
+  
+  // task5 instantiation
+  task5(.newGame_L(newGame_L), 
          .clock(CLOCK_50), 
          .h3(h_3), 
          .h2(h_2), 
@@ -31,37 +51,18 @@ module chipInterface
          .c2(c_2), 
          .c1(c_1), 
          .c0(c_0), 
-         .enter_L(KEY[3]), 
+         .enter_L(enter_L), 
          .reset(SW[17]),
          .hMove(SW[3:0]), 
          .cMove(LEDR[3:0]), 
          .win(ww));
-
+  
+  // LEDG display
   assign LEDG = {ww, ww, ww, ww, ww, ww, ww, ww};
+
+  // HEX display
   logic [7:0] blank;
   assign blank = 8'b00000000;
-  logic reset;
-  logic a1, a2, b1, b2;
-  assign reset = 0;
-  dFlipFlopcI ff0(.d(KEY[3]),
-                .q(a1),
-                .clock(CLOCK_50),
-                .reset(reset)),
-              ff1(.d(a1),
-                .q(a2),
-                .clock(CLOCK_50),
-                .reset(reset));
-  dFlipFlopcI ff2(.d(KEY[3]),
-                .q(b1),
-                .clock(CLOCK_50),
-                .reset(reset)),
-              ff3(.d(b1),
-                .q(b2),
-                .clock(CLOCK_50),
-                .reset(reset));
-
-  
-
   SevenSegmentDisplay(.BCX0(h_3), .blank(blank), .HEX0(HEX7));
   SevenSegmentDisplay(.BCX0(h_2), .blank(blank), .HEX0(HEX6));
   SevenSegmentDisplay(.BCX0(h_1), .blank(blank), .HEX0(HEX5));
@@ -70,6 +71,5 @@ module chipInterface
   SevenSegmentDisplay(.BCX0(c_2), .blank(blank), .HEX0(HEX2));
   SevenSegmentDisplay(.BCX0(c_1), .blank(blank), .HEX0(HEX1));
   SevenSegmentDisplay(.BCX0(c_0), .blank(blank), .HEX0(HEX0));
-
 
 endmodule: chipInterface 
